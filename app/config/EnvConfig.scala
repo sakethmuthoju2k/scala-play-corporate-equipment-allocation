7889ba2a8cf287eadffe6d8bca6aab4b3bb8d410
sakethmuthoju2k <muthojusaketh007@gmail.com>
2024-11-22 15:29:32 +0530

    added envconfig

--- app/config/EnvConfig.scala
+++ app/config/EnvConfig.scala
@@ -2,4 +2,6 @@ package config
 
 object EnvConfig {
   def getKafkaBroker: String = sys.env.getOrElse("KAFKA_BROKERS", "localhost:9092")
+  def getJwtUtilSecretKey: String = sys.env.getOrElse("JWT_UTIL_SECRET_KEY", "corporate-equip-jwt-key")
+  def getJwtUtilIssuer: String = sys.env.getOrElse("JWT_UTIL_ISSUER", "corporate-equip-play-service")
 }
